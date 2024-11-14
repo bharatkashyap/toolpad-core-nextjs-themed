9336f5753d383c761f469ac3b827423e2c1686eb
Bharat Kashyap <bharatkashyap@outlook.com>
2024-11-14 23:02:54 +0530

    wip: Try without auth

--- middleware.ts
+++ middleware.ts
@@ -7,20 +7,20 @@ export default auth((req: NextRequest) => {
   const isLoggedIn = !!req.auth;
   const isAuthPage = req.nextUrl.pathname.startsWith("/auth");
 
-  if (!isLoggedIn && !isAuthPage) {
-    const authUrl = new URL(
-      `${process.env.BASE_PATH}/auth/signin`,
-      process.env.BASE_URL
-    );
-    authUrl.searchParams.set("callbackUrl", req.url);
-    return NextResponse.redirect(authUrl);
-  }
+  // if (!isLoggedIn && !isAuthPage) {
+  //   const authUrl = new URL(
+  //     `${process.env.BASE_PATH}/auth/signin`,
+  //     process.env.BASE_URL
+  //   );
+  //   authUrl.searchParams.set("callbackUrl", req.url);
+  //   return NextResponse.redirect(authUrl);
+  // }
 
-  if (isLoggedIn && isAuthPage) {
-    return NextResponse.redirect(
-      new URL(process.env.BASE_PATH || "/", process.env.BASE_URL)
-    );
-  }
+  // if (isLoggedIn && isAuthPage) {
+  //   return NextResponse.redirect(
+  //     new URL(process.env.BASE_PATH || "/", process.env.BASE_URL)
+  //   );
+  // }
 
   return NextResponse.next();
 });
