74de8d79796c577d3d40895a62306e111475fee1
Bharat Kashyap <bharatkashyap@outlook.com>
2024-11-15 02:14:43 +0530

    wip: Try this

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+// A hack to trigger a refresh upon sign in
+// This is only needed since this app is hosted under a proxy
+// The `redirect` function doesn't trigger a fetch of the component bundle
+// and causes a 404 to appear
+// Omitted from the actual example
+export default function NotFound() {
+  const url = new URL(window.location.href);
+  const callbackUrl = url.searchParams.get("callbackUrl") || "/";
+  return redirect(callbackUrl);
+}
--- middleware.ts
+++ middleware.ts
@@ -5,22 +5,30 @@ import type { NextRequest } from "next/server";
 export default auth((req: NextRequest) => {
   // @ts-ignore
   const isLoggedIn = !!req.auth;
-  const isAuthPage = req.nextUrl.pathname.startsWith("/auth");
+  const isAuthPage = req.nextUrl.pathname.startsWith("/auth/signin");
 
-  // if (!isLoggedIn && !isAuthPage) {
-  //   const authUrl = new URL(
-  //     `${process.env.BASE_PATH}/auth/signin`,
-  //     process.env.BASE_URL
-  //   );
-  //   authUrl.searchParams.set("callbackUrl", req.url);
-  //   return NextResponse.redirect(authUrl);
-  // }
+  if (!isLoggedIn && !isAuthPage) {
+    const authUrl = new URL(
+      `${process.env.BASE_PATH}/auth/signin`,
+      process.env.BASE_URL
+    );
 
-  // if (isLoggedIn && isAuthPage) {
-  //   return NextResponse.redirect(
-  //     new URL(process.env.BASE_PATH || "/", process.env.BASE_URL)
-  //   );
-  // }
+    const lastSegment = req.nextUrl.pathname.split("/").pop();
+
+    const fullUrl = new URL(
+      `${process.env.BASE_PATH || ""}/${lastSegment}`,
+      process.env.BASE_URL
+    );
+
+    authUrl.searchParams.set("callbackUrl", fullUrl.href);
+    return NextResponse.redirect(authUrl);
+  }
+
+  if (isLoggedIn && isAuthPage) {
+    return NextResponse.redirect(
+      new URL(process.env.BASE_PATH || "/", process.env.BASE_URL)
+    );
+  }
 
   return NextResponse.next();
 });
