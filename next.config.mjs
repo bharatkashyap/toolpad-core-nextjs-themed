e1d01cd967406c226143e708aa973472850c8c04
Bharat Kashyap <bharatkashyap@outlook.com>
2024-11-15 12:07:28 +0530

    wip: Try base path again

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ export default (phase) => {
    * @type {import('next').NextConfig}
    */
   const nextConfig = {
+    basePath: process.env.BASE_PATH,
     assetPrefix: isDev
       ? undefined
       : `${process.env.BASE_URL}${process.env.BASE_PATH}`,
