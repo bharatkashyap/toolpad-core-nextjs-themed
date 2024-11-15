34a229476814a242086fba437e04f23eaba89aff
Bharat Kashyap <bharatkashyap@outlook.com>
2024-11-15 12:43:05 +0530

    fix: Invalid server actions error

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,16 @@ export default (phase) => {
     assetPrefix: isDev
       ? undefined
       : `${process.env.BASE_URL}${process.env.BASE_PATH}`,
+    experimental: {
+      serverActions: {
+        // edit: updated to new key. Was previously `allowedForwardedHosts`
+        allowedOrigins: [
+          "https://deploy-preview-4415--mui-toolpad-docs.netlify.app",
+          "https://mui-toolpad-docs.netlify.app",
+          "https://mui.com",
+        ],
+      },
+    },
   };
   return nextConfig;
 };
