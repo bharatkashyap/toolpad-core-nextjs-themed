3dab5c81e52a7d5565c8334ce431ddeed8ef7198
Bharat Kashyap <bharatkashyap@outlook.com>
2025-03-19 13:06:26 +0530

    feat: Use Crud component

--- app/components/CustomDataGrid.tsx
+++ app/components/CustomDataGrid.tsx
@@ -1,24 +1,35 @@
-import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-import { columns, rows } from '../mocks/gridOrdersData';
+import * as React from "react";
+import { DataGrid } from "@mui/x-data-grid";
 
-export default function CustomizedDataGrid() {
+export default function CustomizedDataGrid({
+  rows,
+  columns,
+}: {
+  rows?: any;
+  columns?: any;
+}) {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <DataGrid
         checkboxSelection
         rows={rows}
         columns={columns}
-        getRowClassName={(params) => (params.indexRelativeToCurrentPage % 2 === 0 ? 'even' : 'odd')}
+        getRowClassName={(params) =>
+          params.indexRelativeToCurrentPage % 2 === 0 ? "even" : "odd"
+        }
         initialState={{
           pagination: { paginationModel: { pageSize: 20 } },
         }}
         sx={(theme) => ({
           borderColor:
-            theme.palette.mode === 'dark' ? theme.palette.grey[700] : theme.palette.grey[200],
-          '& .MuiDataGrid-cell': {
+            theme.palette.mode === "dark"
+              ? theme.palette.grey[700]
+              : theme.palette.grey[200],
+          "& .MuiDataGrid-cell": {
             borderColor:
-              theme.palette.mode === 'dark' ? theme.palette.grey[700] : theme.palette.grey[200],
+              theme.palette.mode === "dark"
+                ? theme.palette.grey[700]
+                : theme.palette.grey[200],
           },
         })}
         pageSizeOptions={[10, 20, 50]}
@@ -26,25 +37,31 @@ export default function CustomizedDataGrid() {
         density="compact"
         slotProps={{
           filterPanel: {
+            sx: {
+              "& .MuiDataGrid-filterForm": {
+                columnGap: 1.5,
+                marginTop: 2,
+              },
+            },
             filterFormProps: {
               logicOperatorInputProps: {
-                variant: 'outlined',
-                size: 'small',
+                variant: "outlined",
+                size: "small",
               },
               columnInputProps: {
-                variant: 'outlined',
-                size: 'small',
-                sx: { mt: 'auto' },
+                variant: "outlined",
+                size: "small",
+                sx: { mt: "auto" },
               },
               operatorInputProps: {
-                variant: 'outlined',
-                size: 'small',
-                sx: { mt: 'auto' },
+                variant: "outlined",
+                size: "small",
+                sx: { mt: "auto" },
               },
               valueInputProps: {
                 InputComponentProps: {
-                  variant: 'outlined',
-                  size: 'small',
+                  variant: "outlined",
+                  size: "small",
                 },
               },
             },
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import type { Metadata } from "next";
 import { NextAppProvider } from "@toolpad/core/nextjs";
+import PersonIcon from "@mui/icons-material/Person";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
@@ -23,6 +24,12 @@ const NAVIGATION: Navigation = [
     segment: "toolpad/core/templates/nextjs-dashboard/orders",
     title: "Orders",
     icon: <ShoppingCartIcon />,
+  },
+  {
+    segment: "employees",
+    title: "Employees",
+    icon: <PersonIcon />,
+    pattern: "employees{/:employeeId}*",
   },
 ];
 
